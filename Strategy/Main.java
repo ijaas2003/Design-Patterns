d7672ec05ce289e58f792bc3d9ea00ae07c71804
ijaas2003 <ijaasahamad2003@gmail.com>
2025-11-15 13:04:29 +0530

    Main method implementaion

--- Strategy/Main.java
+++ Strategy/Main.java
@@ -1,6 +1,11 @@
+import Service.PaymentService;
+import Strategys.CreditPayment;
 
 public class Main {
   public static void main(String[] args) {
-
+    System.out.println("Amount::::");
+    double amount = 1999.0;
+    PaymentService paymentService = new PaymentService(new CreditPayment());
+    paymentService.pay(amount);
   }
 }
