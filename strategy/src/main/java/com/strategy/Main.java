af7154297b17e1800170b73d1688bcd8a5fcfde4
ijaas2003 <ijaasahamad2003@gmail.com>
2025-11-15 15:44:00 +0530

    Changes updated

--- strategy/src/main/java/com/strategy/Main.java
+++ strategy/src/main/java/com/strategy/Main.java
@@ -1,7 +1,12 @@
 package com.strategy;
 
+import com.Payments.DebitTransaction;
+import com.Payments.PaymentService;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+    PaymentService service = new PaymentService(new DebitTransaction());
+    service.pay(1000.0);
+  }
 }
